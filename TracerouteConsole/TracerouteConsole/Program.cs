9cf2db181c512c9b587d700cd4d5216d14409afc
Pavel Kalionov <Yrado94@hotmail.com>
2017-10-23 01:46:06 +0300

    1

---
+++ TracerouteConsole/TracerouteConsole/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading;
+
+
+namespace TracerouteConsole
+{
+    class Program
+    {
+        static string _fileName = "D:\\tmp.txt";
+        static void Main(string[] args)
+        {
+            FileStream FS = new FileStream(_fileName, FileMode.OpenOrCreate, FileAccess.Write);
+            while (true)
+            {
+                using (StreamWriter SW = new StreamWriter(FS))
+                {
+
+                    var sites = new List<Task>();
+                    sites.Add(MyTraceRoute.PrintTraceAsync("mail.ru", SW));
+                    sites.Add(MyTraceRoute.PrintTraceAsync("ya.ru", SW));
+                    sites.Add(MyTraceRoute.PrintTraceAsync("vk.com", SW));
+                    sites.Add(MyTraceRoute.PrintTraceAsync("Google.com", SW));
+                }
+                Thread.Sleep(120000);
+            }
+        }
+    }
+
+    public class MyTraceRoute
+    {
+        private const string Data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+
+        public static async Task PrintTraceAsync(string hostNameOrAddress, StreamWriter writer)
+        {
+            await PrintTrace(hostNameOrAddress, writer);
+        }
+
+        private static Task PrintTrace(string hostNameOrAddress, StreamWriter writer)
+        {
+            return Task.Run( () =>
+            {
+                int i = 1;
+                foreach (var ip in MyTraceRoute.GetTraceRoute(hostNameOrAddress))
+                {
+                    writer.WriteLineAsync(i.ToString()+ ", " + hostNameOrAddress + ", " + ip.ToString());
+                    i++;
+                }
+            });
+        }
+
+        private static IEnumerable<IPAddress> GetTraceRoute(string hostNameOrAddress)
+        {
+            for (int ttl = 1;; ttl++)
+            {
+                PingReply reply=default(PingReply);
+                try
+                {
+                    Ping pinger = new Ping();
+                    PingOptions pingerOptions = new PingOptions(ttl, true);
+                    int timeout = 10000;
+                    byte[] buffer = Encoding.ASCII.GetBytes(Data);
+
+                    reply = pinger.Send(hostNameOrAddress, timeout, buffer, pingerOptions);
+                }
+                catch (PingException e)
+                {
+                    Console.WriteLine("{0} Exception caught.", e);
+                    break;
+                }
+
+
+                if (reply.Status == IPStatus.Success)
+                {
+                    yield return reply.Address;
+                    break;
+                }
+                else if (reply.Status == IPStatus.TtlExpired || reply.Status == IPStatus.TimedOut)
+                {
+                    if (reply.Status == IPStatus.TtlExpired)
+                        yield return reply.Address;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
